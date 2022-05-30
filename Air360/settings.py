86e010bfb3ac7db1f5131702611eefa53ae53ca4
Md Risfat <Risfat.bd@gmail.com>
2022-05-30 11:25:06 +0600

    Minor Bug Fixed & Module Updated

--- Air360/settings.py
+++ Air360/settings.py
@@ -146,5 +146,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'risfat.core@gmail.com'
-EMAIL_HOST_PASSWORD = "uqvsvoujtigcbjviu"
+EMAIL_HOST_PASSWORD = "cqnptpjgsocbhohp"
 DEFAULT_FROM_EMAIL = 'Air360 <risfat.core@gmail.com>'
